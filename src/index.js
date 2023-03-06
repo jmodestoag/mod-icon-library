1dbd089f820d6d0eca0daa9439a79a18de653ed4
jmodestoag <alvarezgomezmodesto@gmail.com>
2023-03-06 16:36:28 -0700

    version 2.1.11

--- src/index.js
+++ src/index.js
@@ -7,13 +7,15 @@ import { ReactComponent as IconoTicketPlus } from './icons/ticketPlus.svg'
 import { ReactComponent as IconoTicketPlusCircular } from './icons/ticketPlusCircular.svg'
 import { ReactComponent as IconoChatRead } from './icons/chatRead.svg'
 import { ReactComponent as IconoChatUnread } from './icons/chatUnread.svg'
+import {ReactComponent as IconDelete} from './icons/deleteIcon.svg'
 
 const iconos = {
   notificaciones: IconoNotificaciones,
   ticketPlus: IconoTicketPlus,
   ticketPlusCircular: IconoTicketPlusCircular,
   IconoChatUnread: IconoChatUnread,
-  IconoChatRead: IconoChatRead
+  IconoChatRead: IconoChatRead,
+  IconDelete: IconDelete
 }
 
 const Icon = modLibreriaIconos(iconos)
